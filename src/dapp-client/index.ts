e15ae4e12f9bf173edf8ff7dc5cf62786be06a22
lohuza <lasha.abazashvili@gmail.com>
2023-11-25 12:47:36 +0400

    dapp

--- src/dapp-client/index.ts
+++ src/dapp-client/index.ts
@@ -4,12 +4,20 @@ import { DappWs } from "../ws";
 
 export class DappClient {
   private readonly ws: DappWs;
-  public api: DappApi;
+  private api: DappApi;
   public onMessage: (msg: SocketMessage) => {};
 
   constructor(config: PlenaConnectConfig) {
     this.api = new DappApi(config.bridge, config.sessionId, config.dappToken);
     this.ws = new DappWs(`${config.wsUrl}/dapp-ws?token=${config.dappToken}&sessionid=${config.dappSessionId}`, this);
+  }
+
+  sendTransaction(chain: number, method: string, data: any) {
+    return this.api.sendTransaction(chain, method, data)
+  }
+
+  cancelTransaction(transactionId: string) {
+    return this.api.cancelTransaction(transactionId);
   }
 
   closeConnection() {
